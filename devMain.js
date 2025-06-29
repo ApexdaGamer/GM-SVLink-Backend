d38daa71ad6a40840d7dbca33dd2b61de774c599
ApexdaGamer <mikufanh@gmail.com>
2025-06-28 18:58:08 -0700

    update devmain + udp dev copy
    stonks

--- devMain.js
+++ devMain.js
@@ -2,20 +2,29 @@ const WebSocket = require('ws');
 const bodyParser = require('body-parser');
 
 const wss = new WebSocket.Server({port: 7774})
-
+const sids = {}
 let csid = 0
+
+const gkbv = (object, value) => {return Object.keys(object).find(key => object[key] === value)}
 
 wss.on('connection', async (ws) => {
     ws.on('message', (msg) => {
-        console.log("received message '" + msg + "'")
-        //if (msg.startsWith("direct")) {
-        //    const sid = Number(msg.substring(7,8))
-        //}
-        wss.clients.forEach((client) => {
-            if (client != ws && client.readyState != 2) {
-                client.send(msg)
+        // console.log("received message '" + msg + "'")
+        // this was slowing down the server with the sheer amount of text
+        if (msg.startsWith("direct")) {
+            const args = msg.split(" ")
+            args.shift()
+            const sid = args.shift()
+            if (sids[sid].readyState == 1) {
+                sids[sid].send(`${sid} ${args.join(" ")}`)
             }
-        })
+        } else {
+            wss.clients.forEach((client) => {
+                if (client != ws && client.readyState == 1) {
+                    client.send(gkbv(sids, ws) + " " + msg)
+                }
+            })
+        }
     })
 	ws.on("error", function(err) {
 		console.log("connection error")
@@ -23,10 +32,17 @@ wss.on('connection', async (ws) => {
 	});
     ws.on('close', () => {
         console.log("connection closed")
+        wss.clients.forEach((client) => {
+            if (client != ws && client.readyState == 1) {
+                client.send(gkbv(sids, ws) + " svd")
+            }
+        })
     })
-    ws.send("sid " + csid.toString())
-    csid += 1
     console.log("connection accepted")
+    ws.send("-1 sid " + csid.toString())
+    sids[csid.toString()] = ws
+    console.log("sid assigned: " + csid.toString())
+    csid += 1
 })
 
 wss.on('listening', () => {
---
+++ devMainUDP.js
@@ -0,0 +1,78 @@
+const udp = require('dgram');
+const server = udp.createSocket('udp4');
+
+const gkfv = (object, value) => {return Object.keys(object).find(key => object[key] === value)}
+
+const cs = {}
+let csid = 0
+
+const ef = (err) => {
+    if (error) {
+        console.log(`connection errored: ${err}`)
+        let sid = -1
+        Object.values(cs).forEach(c => {
+            if (c.a == info.address && c.p == info.port) {
+                sid = gkfv(cs, c)
+                cs[sid] = null
+            }
+        });
+        console.log(`connection error handled for server ${sid}`)
+    }
+}
+
+server.on('error', function(error){
+    console.log('UDP Server error: ' + error);
+    server.close();
+});
+
+server.on('message', function(msg,info) {
+    msg = msg.toString()
+    console.log(`received msg '${msg}'`);
+    if (msg == "sup") {
+        console.log("connection accepted")
+        cs[csid] = {
+            p: info.port,
+            a: info.address
+        }
+        server.send(Buffer.from(`-1 sid ${csid}`), info.port, info.address, ef)
+        console.log(`sid assigned: ${csid}`)
+        csid += 1
+    } else if (msg == "bye") {
+        console.log("connection closed")
+        let sid = -1
+        Object.values(cs).forEach(c => {
+            if (c.a == info.address && c.p == info.port) {
+                sid = gkfv(cs, c)
+                cs[sid] = null
+            }
+        });
+        Object.values(cs).forEach(c => {
+            server.send(Buffer.from(`${sid} svd`), c.p, c.a, ef)
+        })
+        console.log(`disconnection handled for server ${sid}`)
+    } else if (msg.startsWith("direct")) {
+        const args = msg.split(" ")
+        args.shift()
+        const sid = args.shift()
+        const c = cs[sid]
+        if (c)
+            server.send(Buffer.from(`${sid} ${args.join(" ")}`), c.p, c.a, ef)
+    } else {
+        Object.values(cs).forEach(c => {
+            if (c.p != info.port && c.a != info.address) 
+                server.send(Buffer.from(`${gkfv(cs, c)} ${msg}`), c.p, c.a, ef)
+        })
+    }
+});
+
+server.on('listening',function(){
+  var address = server.address();
+  var port = address.port;
+  console.log('UDP Server running on port ' + port);
+});
+
+server.on('close',function(){
+  console.log('UDP Server closed');
+});
+
+server.bind(7772);
