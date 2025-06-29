7d068738c3bc5552eb4e8364a94cc5b3c74028b6
ApexdaGamer <mikufanh@gmail.com>
2025-06-29 15:27:21 -0700

    maybe i can
    maybe it was that msg isn't a string for some reason....
    
    ~~it even says it in the docs~~

--- devMain.js
+++ devMain.js
@@ -8,9 +8,10 @@ const gkbv = (object, value) => {return Object.keys(object).find(key => object[k
 
 wss.on('connection', async (ws) => {
     ws.on('message', (msg) => {
+        msg = msg.toString()
         // console.log("received message '" + msg + "'")
         // this was slowing down the server with the sheer amount of text
-        if (msg.substring(0, 6) == "direct") {
+        if (msg.startsWith("direct")) {
             const args = msg.split(" ")
             args.shift()
             const sid = args.shift()
@@ -20,7 +21,7 @@ wss.on('connection', async (ws) => {
         } else {
             wss.clients.forEach((client) => {
                 if (client != ws && client.readyState == 1) {
-                    client.send(gkbv(sids, ws) + " " + msg)
+                    client.send(`${gkbv(sids, ws)} ${msg}`)
                 }
             })
         }
@@ -28,17 +29,22 @@ wss.on('connection', async (ws) => {
 	ws.on("error", function(err) {
 		console.log("connection error")
 		console.log(err.stack)
+        wss.clients.forEach((client) => {
+            if (client != ws && client.readyState == 1) {
+                client.send(`${gkbv(sids, ws)} svd`)
+            }
+        })
 	});
     ws.on('close', () => {
         console.log("connection closed")
         wss.clients.forEach((client) => {
             if (client != ws && client.readyState == 1) {
-                client.send(gkbv(sids, ws) + " svd")
+                client.send(`${gkbv(sids, ws)} svd`)
             }
         })
     })
     console.log("connection accepted")
-    ws.send("-1 sid " + csid.toString())
+    ws.send(`-1 sid ${csid}`)
     sids[csid.toString()] = ws
     console.log("sid assigned: " + csid.toString())
     csid += 1
